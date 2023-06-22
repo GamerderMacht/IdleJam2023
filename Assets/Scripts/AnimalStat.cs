573d9fb259b6b8d28c9301584f82ae9d40298c82
Andrew <64059280+lilman20010@users.noreply.github.com>
2023-06-22 17:52:09 -0400

    Added Script for Animal Bonuses
    Scripts can be attached to animals in order to provide benefits to player

---
+++ Assets/Scripts/AnimalStat.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalStat : MonoBehaviour
+{
+    AnimalStatBonus animalStatBonus;
+    bool mutation;
+    public float multiplierBonus;
+    public float afkTimerBonus;
+    public float mutationMultiplierBonus;
+    public float mutationAfkTimerBonus;
+    void Start()
+    {
+        animalStatBonus = FindObjectOfType<AnimalStatBonus>();
+        ApplyAnimalBonus();
+    }
+
+    void ApplyAnimalBonus()
+    {
+        animalStatBonus.coinProductionMultiplier += multiplierBonus;
+        animalStatBonus.coinProductionSpeedDecrease += afkTimerBonus;
+    }
+
+    void ApplyAnimalMutationBonus()
+    {
+        if (mutation)
+        {
+            animalStatBonus.coinProductionMultiplier += mutationMultiplierBonus;
+            animalStatBonus.coinProductionSpeedDecrease += mutationAfkTimerBonus;
+        }
+    }
+
+}
