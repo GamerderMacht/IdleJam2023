4b370138e95b104182e7d8bc88c91cf251e604b5
Andrew <64059280+lilman20010@users.noreply.github.com>
2023-06-26 17:31:50 -0400

    Removed unecceacry text

--- Assets/Scripts/ShopPrices.cs
+++ Assets/Scripts/ShopPrices.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShopPrices : MonoBehaviour
 {
+    public TextMeshPro[] priceText;
     public int[] prices;
+
     
     void Start()
     {
